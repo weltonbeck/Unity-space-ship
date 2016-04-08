fc8354b7fa7b8e606c5602bc8c3d7b2973211543
Welton Beck <weltonbeck@Weltons-iMac.local>
2016-04-08 13:55:53 -0300

    aula 2

---
+++ Aula 2/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (BoxCollider2D))]
+public class Player : MonoBehaviour {
+
+	// variavel que determina a minha velocidade
+	float speed = 10f;
+
+	// Uesta função é chamada apenas quando inicia
+	void Start () {
+	
+	}
+	
+	// Esta função é chamada a cada ciclo
+	void Update () {
+
+		// Pego o movimento horizontal do controle
+		// ele me retorna de -1 a +1 sendo 0 o estado parado
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		// pego o movimento vertical
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// Variavel do tipo Vector2 sendo os valores referente aos eixos X e Y
+		// multiplico o valor do meu movimentoHorizontal pelo speed para determinar a velocidade do deslocamento.
+		Vector2 movement = new Vector2(moveHorizontal * speed, moveVertical * speed);
+
+		// Adiciono uma velocidade a meu player
+		// GetComponent faz com que eu pegue algum component ou script inserido no meu objeto(prefab)
+		// O velocity aplica o deslocamento apenas enquanto a força é aplicada, assim que parar de receber esta força o deslocamento tambem para.
+		 GetComponent<Rigidbody2D> ().velocity = movement;
+
+		/*
+		 * Outra forma de deslocar um objeto é inserindo uma força a ele, mas este funciona mais como um empurrão 
+		 * que tem um grande impacto no começo e depois vai perdendo sua força, usado geralmente para pulos em jogos de plataforma.
+		 *  
+		 *	GetComponent<Rigidbody2D> ().AddForce ( new Vector2( 10f, 0f ) );
+		 */
+
+	}
+
+}
